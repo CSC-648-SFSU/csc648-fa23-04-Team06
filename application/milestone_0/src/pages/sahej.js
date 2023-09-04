32c3ce083ec143ea8e341db2445dc705f231a6cc
sahejt <hello@sahejtuli.com>
2023-09-03 20:07:21 -0700

    Title added for Sahej's page

--- application/milestone_0/src/pages/sahej.js
+++ application/milestone_0/src/pages/sahej.js
@@ -4,7 +4,7 @@ export default function main(){
     return(
         <>
         {/* Feel free to use the Header component to add a subHeader, use prop as I have used in index.js... - ST */}
-        <Header headerText="Sahej Tuli"/>
+        <Header headerText={"Sahej Tuli"} subheaderText={"Team Lead"}/>
         </>
     )
 }
