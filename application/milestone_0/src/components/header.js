dd8a602872132c21794a09b6921395e58eab7efc
Sahej Tuli <sahejtuli@sfs-wifi-dhcp-10-143-143-4.wifi.sfsu.edu>
2023-09-08 12:04:57 -0700

    Basic Project Structure with pages/routes added

---
+++ application/milestone_0/src/components/header.js
@@ -0,0 +1,5 @@
+export default function header(){
+    return(
+        <h1>CSC-648-04, Team 6</h1>
+    )
+}
---
+++ application/milestone_0/src/pages/navjot.js
@@ -0,0 +1,10 @@
+import Header from "../components/header"
+
+export default function(){
+    return(
+        <>
+        <Header/>
+        <h2>Navjot's Page</h2>
+        </>
+    )
+}
