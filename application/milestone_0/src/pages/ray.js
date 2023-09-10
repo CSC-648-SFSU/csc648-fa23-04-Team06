7e40b8649c978347558371651c0d6485c8e770d7
trmbeefflavor <rdelacruz@sfsu.edu>
2023-09-10 05:53:50 -0700

    finished efx

--- application/milestone_0/src/pages/ray.js
+++ application/milestone_0/src/pages/ray.js
@@ -12,7 +12,6 @@ export default function(){
         </div>
         <div className="flex-container">
             <div className="flex-item">
-                <h2 className="item-title">About Me</h2>
                 <div className="item-content-layout">
                     <div className="item-content-img">
                         <img
@@ -21,41 +20,34 @@ export default function(){
                           style={{ width: '281px', height: '400px' }}
                         />
                     </div>
-                    <div className="item-content">
-                        <p className="reg-text">
-                          Hi I'm Ray and currently a Senior and scheduled 
-                          to graduate sprint 2024. I am a transfer from Napa
-                          Valley College. Going in to this field is a career
-                          change from my work in mental health for the state.
-                          It has proven to be a big change from what I have 
-                          been used to but, it has been a welcome challenge.
-                        </p>
-                    </div> 
                 </div>
             </div>
-            <div className="flex-item">
-                <h2 className="item-title">Hobbies</h2>
-                <div className="item-content-layout">
-                    <div className="item-content">                    
-                        <ul className="hobby-text">
-                            <li>ergpj</li>
-                            <li>ergpj</li>
-                            <li>ergpj</li>
-                            <li>ergpj</li>
-                        </ul>
-                    </div>
-                </div>
+            <div className="flex-item">               
+                    <h2 className="item-title">About Me</h2>
+                <div className="item-content">
+                    <p className="reg-text">
+                        Hi I'm Ray and currently a Senior and scheduled 
+                        to graduate sprint 2024. I am a transfer from Napa
+                        Valley College. Going in to this field is a career
+                        change from my work in mental health for the state.
+                        It has proven to be a big change from what I have 
+                        been used to but, it has been a welcome challenge.
+                    </p>
+                    <p className="reg-text">
+                        My interests outside of studying include video games
+                         (playing Baldur's Gate 3). I like to watch
+                          K/C dramas (watching A Time Called You). 
+                          I also like to attend concerts (attending Hanabie 
+                          next month).          
+                    </p>
+                    <p className="reg-text">
+                        My goals after I graduate is to work somewhere I 
+                        will be equally confident at like my former career 
+                        with a little more success.
+                    </p>
+                </div>        
             </div>
-            <div className="flex-item">
-                <h2 className="item-title">Section</h2>
-            </div>
-
         </div>
-            
-        
-                
-        
-
         </>
     )
 }
