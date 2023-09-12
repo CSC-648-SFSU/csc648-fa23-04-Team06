2a231d7fb682e6eb5f7522b74fe64d7be0371f79
Jess Rosero <ic3b34rgames@gmail.com>
2023-09-11 19:55:58 -0700

    Created Jessica's Base Page
    - Added basic page requirements
    - Added profile image
    ** need to add more creative elements **

--- application/milestone_0/src/pages/jessica.js
+++ application/milestone_0/src/pages/jessica.js
@@ -1,10 +1,22 @@
 import Header from "../components/header"
+import jessPFP from "../images/jessPFP.jpg"
+import "../styles/jessica.css"
 
 export default function main(){
     return(
         <>
         {/* Feel free to use the Header component to add a subHeader, use prop as I have used in index.js... - ST */}
-        <Header headerText="Jessica Christine Rosero"/>
+        <Header headerText="Jessica Christine Rosero" subheaderText="Back-end Team Lead"/>
+        <div className = "intro">
+        <img src = {jessPFP} alt = "Jessica Christine Rosero" width = "300" class = "img"/>
+            <div className = "intro-text">
+                <p>Hello! My name is Jessica Christine Rosero, or just Jess. I am a senior at San Francisco State University majoring in Computer Science.
+                When I'm not working away at the plethora of schoolwork and projects that I have, I enjoy playing video games like TFT and The Legend of Zelda: Tears Of The Kingdom! I can't wait
+                to work on amazing games like these someday!
+                </p>
+            </div>
+        </div>
         </>
     )
+
 }
