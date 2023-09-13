62f509282a6e346d1d3ee8f1a8a9c36d4060e973
Ryan Tong <91310781+Anonynthesis@users.noreply.github.com>
2023-09-13 13:24:28 -0700

    updated className in ryan' page

--- application/milestone_0/src/pages/ryan.js
+++ application/milestone_0/src/pages/ryan.js
@@ -4,13 +4,13 @@ import "../styles/ryan.css"
 
 export default function main(){
     return(
-        <div className="intro">
+        <div className="intro-ryan">
           {/* Feel free to use the Header component to add a subHeader, use prop as I have used in index.js... - ST */}
           <Header headerText={"Ryan Tong"} subheaderText={"Full-Stack Developer"}/>
 
           <img src={RyanPhoto} alt="Ryan Tong" />
 
-          <p className="intro-text">
+          <p>
             Hello, I'm Ryan Tong. I am currently a senior at San Francisco State University majoring in computer science.
             I'm currently deciding if I want to go into Databases, Artificial Intelligence, or Game Development.
             My hobbies board games(Monopoly, Chess) and video games(League, TFT, recently got back into ROTMG) with friends, 
