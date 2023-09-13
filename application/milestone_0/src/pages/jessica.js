3305d73208a9f25a8d4384410d9e3b715d37e89a
Jess Rosero <ic3b34rgames@gmail.com>
2023-09-12 22:41:47 -0700

    changed class names
    - changed to unique class names

--- application/milestone_0/src/pages/jessica.js
+++ application/milestone_0/src/pages/jessica.js
@@ -7,8 +7,8 @@ export default function main(){
         <>
         {/* Feel free to use the Header component to add a subHeader, use prop as I have used in index.js... - ST */}
         <Header headerText="Jessica Christine Rosero" subheaderText="Back-end Team Lead"/>
-        <div className = "intro">
-        <img src = {jessPFP} alt = "Jessica Christine Rosero" class = "img"/>
+        <div className = "intro-section-jess">
+        <img src = {jessPFP} alt = "Jessica Christine Rosero" class = "img-jess"/>
             <div className = "intro-text">
                 <p>Hello! My name is Jessica Christine Rosero, or just Jess. I am a senior at San Francisco State University majoring in Computer Science.
                 When I'm not working away at the plethora of schoolwork and projects that I have, I enjoy playing video games like TFT and The Legend of Zelda: Tears Of The Kingdom! I can't wait
