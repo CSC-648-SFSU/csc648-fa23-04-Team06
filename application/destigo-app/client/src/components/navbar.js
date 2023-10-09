2da58cce2fa3320873e56269041099cc20cf8357
sahejt <hello@sahejtuli.com>
2023-10-09 13:23:39 -0700

    Homepage setup

---
+++ application/destigo-app/client/src/components/navbar.js
@@ -0,0 +1,1 @@
+{/* Navjot Singh */}
---
+++ application/destigo-app/client/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function App() {
+    return (
+  <div>
+    {/* Navbar here - Sahej Tuli*/}
+      <div className='header'></div> {/* Navjot Singh */}
+      <div className='description'></div>  {/* Navjot Singh */}
+      <div className='airline-partners'></div>  
+      <div className='hotels-partners'></div>
+      <div className='event-partners'></div>
+  </div>
+  );
+}
