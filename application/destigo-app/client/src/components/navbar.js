405f3eab40d3ebf643b676cc4825f2dd739bc90d
sahejt <hello@sahejtuli.com>
2023-10-09 13:26:13 -0700

    Navbar basic

--- application/destigo-app/client/src/components/navbar.js
+++ application/destigo-app/client/src/components/navbar.js
@@ -1,1 +1,21 @@
-{/* Navjot Singh */}
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="navbar">
+        <li className="navitem">
+          <a href="/">Link</a>
+        </li>
+        <li className="navitem">
+          <a href="/about">Link</a>
+        </li>
+        <li className="navitem">
+          <a href="/contact">Link</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- application/destigo-app/client/src/pages/index.js
+++ application/destigo-app/client/src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Navbar from '../components/navbar';
 
 export default function App() {
     return (
   <div>
-    {/* Navbar here - Sahej Tuli*/}
+    <Navbar/>{/* Navbar here - Sahej Tuli*/}
       <div className='header'></div> {/* Navjot Singh */}
       <div className='description'></div>  {/* Navjot Singh */}
-      <div className='airline-partners'></div>  
+      <div className='airline-partners'></div>
       <div className='hotels-partners'></div>
       <div className='event-partners'></div>
   </div>
