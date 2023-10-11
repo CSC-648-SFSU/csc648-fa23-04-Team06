bb6866e80423b936ce634f224d27fb4c8b48b33c
sahejt <hello@sahejtuli.com>
2023-10-10 23:47:22 -0700

    API Integration

--- application/destigo-app/client/src/utils/fetchApi.js
+++ application/destigo-app/client/src/utils/fetchApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://destigo-app-api-backend.vercel.apps"
+const BASE_URL = "https://destigo-app-api-backend.vercel.app"
 
 export const request = async (url, method, headers = {}, body = {}, isNotStringified = false) => {
     let res
