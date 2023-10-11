22d7b041530d498b5f4f5f4e2f4efd21767ed786
Ryan Tong <91310781+Anonynthesis@users.noreply.github.com>
2023-10-10 19:19:40 -0700

    added description

--- application/destigo-app/client/src/pages/index.js
+++ application/destigo-app/client/src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navbar from '../components/navbar/navbar.js';
 import IntroVideo from '../videos/intro.mp4';
+import DescriptionBackgroundImage from '../images/descriptionBG.webp';
 import '../styles/index.scss'; 
+
 
 export default function App() {
   return (
@@ -18,7 +20,25 @@ export default function App() {
           <h3> Travel with ease, all in one place.</h3>
         </div>
       </div>
-      <div className='description'></div>
+      <div className='description'>
+        <img className='descriptionBG' src={DescriptionBackgroundImage} alt="descriptionBG"/>
+        
+        <div className='description-text'>
+          <b className='title'>WHAT IS DESTIGO?</b>
+
+          <p className='word-description'>
+            "DestiGo" is a revolutionary one-stop travel app that transforms 
+            the way travelers plan and experience their journeys. With “DestiGo”, 
+            users have access to an all-in-one platform that seamlessly integrates 
+            hotels, flights, events, and everything in between. Our app utilizes cutting-edge 
+            technology to offer personalized trip planning, real-time travel updates, 
+            and local insights, all in a single, user-friendly interface. As the travel 
+            industry evolves, “DestiGo” aims to be the ultimate companion for travelers, 
+            simplifying the booking process and enhancing the travel experience, whether 
+            for leisure or business. Travel with ease, all in one place—Travel with “DestiGo”.
+          </p>
+        </div>
+      </div>
       <div className='airline-partners'></div>
       <div className='hotels-partners'></div>
       <div className='event-partners'></div>
