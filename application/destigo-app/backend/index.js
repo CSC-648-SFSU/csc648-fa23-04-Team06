304a1b0969712f7e39c96c96e35fa9b98fbabee8
sahejt <hello@sahejtuli.com>
2023-10-24 11:29:46 -0700

    CORS URL changed

--- application/destigo-app/backend/index.js
+++ application/destigo-app/backend/index.js
@@ -23,8 +23,7 @@ app.get("/", (req, res) => {
 
 //cors 
 const allowedOrigins = [
-  'https://destigo-app-client-frontend.vercel.app', // CORS Rule for Production URL
-  'http://localhost:3000' // CORS Rule for Development (localhost) URL
+  'https://destigo-app-client-frontend.vercel.app' // CORS Rule for Production URL
 ];
 
 const corsOptions = {
