7b813eaa565bae41683d637342992f46b8cfe453
sahejt <hello@sahejtuli.com>
2023-10-30 01:34:24 -0700

    fetchApi.js URL fix

--- application/destigo-app/client/src/utils/fetchApi.js
+++ application/destigo-app/client/src/utils/fetchApi.js
@@ -1,5 +1,4 @@
-// const BASE_URL = "https://destigo-backend.onrender.com"; 
-const BASE_URL = "http://localhost:8800"; 
+const BASE_URL = "https://destigo-backend.onrender.com"; 
 
 export const request = async (
   url,
