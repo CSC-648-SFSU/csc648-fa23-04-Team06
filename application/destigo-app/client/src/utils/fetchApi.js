5e4c7da4c9d2a43e980ca519905915fb9a5bd17d
trmbeefflavor <111946657+trmbeefflavor@users.noreply.github.com>
2023-11-20 20:21:49 -0800

    Update fetchApi.js

--- application/destigo-app/client/src/utils/fetchApi.js
+++ application/destigo-app/client/src/utils/fetchApi.js
@@ -1,5 +1,5 @@
-//const BASE_URL = "https://destigo-backend.onrender.com"; 
-const BASE_URL = "http://localhost:8800"; 
+const BASE_URL = "https://destigo-backend.onrender.com"; 
+//const BASE_URL = "http://localhost:8800"; 
 
 export const request = async (
   url,
