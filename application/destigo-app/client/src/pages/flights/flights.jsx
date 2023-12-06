7188e82d07187aba62fd4ba035c8a881cc08130b
sahejt <hello@sahejtuli.com>
2023-12-05 21:23:24 -0800

    Added content to hotels/flights page + fixed chat colors

--- application/destigo-app/client/src/pages/flights/flights.jsx
+++ application/destigo-app/client/src/pages/flights/flights.jsx
@@ -3,6 +3,41 @@ import Navbar from "../../components/navbar/Navbar";
 import "./flights.css";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/header";
+import ImageColumnDescription from "../../components/imageColumnDescription/imageColumnDescription";
+import ImgDelta from "../../assets/delta.png";
+import ImgEtihad from "../../assets/etihad.png";
+import ImgEmirates from "../../assets/emirates.png";
+import Newsletter from "../../components/newsletter/Newsletter";
+
+const AirlinePartnersData = [
+  {
+    src: ImgEtihad,
+    alt: "Etihad Airways",
+    title: "Etihad Airways",
+    description:
+      "Etihad Airways is the national airline of the United Arab Emirates, renowned for its luxurious travel experience and top-notch service.",
+    button: "Explore Etihad ->",
+    url: "/flights",
+  },
+  {
+    src: ImgDelta,
+    alt: "Delta Airlines",
+    title: "Delta Airlines",
+    description:
+      "Delta Airlines is one of the major American airlines known for its extensive domestic and international flight network. ",
+    button: "Explore Delta ->",
+    url: "/flights",
+  },
+  {
+    src: ImgEmirates,
+    alt: "Emirates",
+    title: "Emirates",
+    description:
+      "Emirates is a premier airline based in Dubai, United Arab Emirates, and is considered one of the largest and most influential carriers globally. ",
+    button: "Explore Emirates ->",
+    url: "/flights",
+  },
+];
 
 const flights = () => {
   return (
@@ -10,8 +45,13 @@ const flights = () => {
       <Navbar />
 
       {/* Calling the header component */}
-      <Header header={"Flights"} />
-      
+      <Header header={"Flights"} subHeader={"Soon, you'll be able to book flights directly through DestiGo!"} />
+      <ImageColumnDescription
+        heading="Our Airline Partners"
+        column={AirlinePartnersData}
+        backgroundColor="#fcf2f2"
+      />
+      <Newsletter/>
       <Footer />
     </>
   );
--- application/destigo-app/client/src/pages/hotels/hotels.jsx
+++ application/destigo-app/client/src/pages/hotels/hotels.jsx
@@ -3,13 +3,57 @@ import Navbar from "../../components/navbar/Navbar";
 import "./hotels.css";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/header";
+import Newsletter from "../../components/newsletter/Newsletter";
+import ImgLondon from "../../assets/london.png";
+import ImgBali from "../../assets/bali.png";
+import ImgParis from "../../assets/paris.png";
+import ImageColumnDescription from "../../components/imageColumnDescription/imageColumnDescription";
+
+const PopularDestinationData = [
+  // This ImageColumnDescription Component is suitable for upto 12 columns,
+  // please don't add more than 12 objects in the array.
+
+  {
+    src: ImgLondon,
+    alt: "London, United Kingdom",
+    title: "London, United Kingdom  🇬🇧",
+    description:
+      "London, a modern metropolis steeped in tradition, offers a dynamic tapestry of experiences—historic landmarks like the Tower of London, green spaces like Hyde Park, and a cultural mosaic seen in its museums, theaters, and markets.",
+    button: "Fly to London ->",
+    url: "/flights",
+  },
+  {
+    src: ImgParis,
+    alt: "Paris, France",
+    title: "Paris, France 🇫🇷",
+    description:
+      "Paris, the City of Light, exudes romance and culture along the Seine River. Iconic landmarks like the Eiffel Tower and Louvre Museum define its skyline. Charming neighborhoods, exquisite cuisine, and a rich artistic heritage make Paris a global treasure.",
+    button: "Fly to Paris ->",
+    url: "/flights",
+  },
+  {
+    src: ImgBali,
+    alt: "Bali, Indonesia",
+    title: "Bali, Indonesia 🇮🇩",
+    description:
+      "Bali, the Island of the Gods, is a tropical haven where lush rice terraces, ancient temples like Uluwatu, and beachside charm in Seminyak converge, inviting visitors to explore a unique blend of spirituality and natural beauty.",
+    button: "Fly to Bali ->",
+    url: "/flights",
+  },
+];
 
 const hotels = () => {
   return (
     <>
       <Navbar />
-      <Header header={"Hotels"} />
+      <Header header={"Hotels"} subHeader={"Soon, you'll be able to book hotels directly through DestiGo!"} />
+      <ImageColumnDescription
+        heading="Popular Destinations"
+        column={PopularDestinationData}
+      />
+      <Newsletter/>
       <Footer />
+      
     </>
   );
 };
